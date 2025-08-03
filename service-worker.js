a836aefad3cd957e5ad86292edf052d433cf46f2
Alimi Azeez Olajide <slideteckacademy@gmail.com>
2025-08-03 06:59:34 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('kitchen-store-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/manifest.json'
+        // Add more assets here
+      ]);
+    })
+  );
+});
 
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
