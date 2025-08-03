7e7e2319139fa695e69b845219b963e2dd592acc
Alimi Azeez Olajide <slideteckacademy@gmail.com>
2025-08-03 16:50:14 +0100

    Update footer-dropdown.js

--- footer-dropdown.js
+++ footer-dropdown.js
@@ -1,20 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const dropdownToggles = document.querySelectorAll(".dropdown-toggle");
-
-  dropdownToggles.forEach(toggle => {
-    toggle.addEventListener("click", function () {
-      const dropdownContent = this.nextElementSibling;
-      const icon = this.querySelector("i");
-
-      if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-        icon.classList.remove("fa-chevron-up");
-        icon.classList.add("fa-chevron-down");
-      } else {
-        dropdownContent.style.display = "block";
-        icon.classList.remove("fa-chevron-down");
-        icon.classList.add("fa-chevron-up");
-      }
+  const toggles = document.querySelectorAll(".dropdown-toggle");
+  toggles.forEach(toggle => {
+    toggle.addEventListener("click", () => {
+      const content = toggle.nextElementSibling;
+      content.classList.toggle("show");
     });
   });
 });
