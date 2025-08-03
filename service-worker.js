eb5c7ba5629a028a4f894b339a88e7270d9aca78
Alimi Azeez Olajide <slideteckacademy@gmail.com>
2025-08-03 15:31:05 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('kitchen-store-v2').then(function(cache) {
+    caches.open('kitchen-store-v3.3').then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html',
