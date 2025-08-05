611c40eb1e7b50b86794b647a00c788407885fbc
Alimi Azeez Olajide <slideteckacademy@gmail.com>
2025-08-05 23:12:45 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kitchen-store-v1.99';
+const CACHE_NAME = 'kitchen-store-v2.82';
 const URLS_TO_CACHE = [
   '/essentialkitchen-culinary/', // root for GitHub Pages
   '/essentialkitchen-culinary/index.html',
